c82012faaa09a208b97001b95ccd48099b218adb
victormusta <victorgrabowski33@gmail.com>
2023-11-16 21:25:12 +0100

    :sparkles: New Compliment feature just popped! todo: add timer to prevent the bot from spamming.

--- functionality/feature_proposals/propose_feature.py
+++ functionality/feature_proposals/propose_feature.py
@@ -1,13 +1,13 @@
 import logging
 
-logger = logging.getLogger('proposalLogger')
-handler = logging.FileHandler(filename='features.log', encoding='utf-8', mode='w')
+logger = logging.getLogger(__name__)
+handler = logging.FileHandler(filename='features.log', encoding='utf-8', mode='a')
 logger.addHandler(handler)
 logger.setLevel(logging.INFO)
 
 
 async def propose_feature(user, content, channel):
-    answer = (f':heart: Thank you @{user} for your proposal! \n I took note of it and you\'ll maybe see it '
-              f'being implemented sooner or later! :heart: \n ')
+    answer = (f':heart: Thank you @{user} for your proposal! :heart: \n I took note of it and you\'ll maybe see it '
+              f'being implemented sooner or later! \n ')
     logger.info(f'NEW_PROPOSAL !:\nUser: {user} \nProposition:{content}')
     await channel.send(answer)
--- start.py
+++ start.py
@@ -2,6 +2,7 @@
 
 import discord
 import logging
+from functionality.Compliments.compliments import compliment_user
 from functionality.feature_proposals.propose_feature import propose_feature
 
 
@@ -9,7 +10,6 @@ def start(token):
 
     intents = discord.Intents.default()
     intents.message_content = True
-
     client = discord.Client(intents=intents)
     handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
     logger = logging.getLogger("__name__")
@@ -24,12 +24,15 @@ def start(token):
     async def on_message(message):
         if message.author == client.user:
             return
-        print(message)
-        print(message.channel.name)
-        if message.content.startswith('$'):
-            await message.channel.send('Hello!')
+        if message.content.startswith('$help'):
+            await message.channel.send('Liste des commandes:')
         if message.channel.name == "new_features":
             await propose_feature(message.author, message.content, message.channel)
-        await message.channel.send('t\'m! :cat:')
-
+        if message.author.name in ["musta33", "AnnBolyn"]:
+            await compliment_user(message.channel, message.author)
+        # problème todo: le client n'as pas accès à tous les utilisateurs, c'est un problème pour get le dernier message de l'utilisateur.
     client.run(token, log_handler=handler, log_level=logging.DEBUG)
+
+
+if __name__ == '__main__':
+    start(BOTTOKEN)
